4a3c06ab56d754f66afb60a17d8ce0a2bc9a8937
SimonGaspar <simon.gaspar@gmail.com>
2019-03-11 20:36:30 +0100

    display point cloud

--- Bakalárska práca/Form1.cs
+++ Bakalárska práca/Form1.cs
@@ -42,40 +42,113 @@ namespace Bakalárska_práca
 
             
         }
-
-        private void ParticleReader()
+        
+        public void ReadPlainText(RenderWindowControl renderWindowControl)
         {
             // Path to vtk data must be set as an environment variable
             // VTK_DATA_ROOT = "C:\VTK\vtkdata-5.8.0"
             vtkTesting test = vtkTesting.New();
             string root = test.GetDataRoot();
-            string filePath = System.IO.Path.Combine(root, @"Data\Particles.raw");
+            string filePath = @"C:\Users\Notebook\Desktop\ImageDataset_SceauxCastle-master\Colors.txt";
 
-            // Read the file
-            vtkParticleReader reader = vtkParticleReader.New();
+            FileStream fs = null;
+            StreamReader sr = null;
+            String sLineBuffer;
+            String[] sXYZ;
+            char[] chDelimiter = new char[] { ' ', '\t', ';' };
+            double[] xyz = new double[10];
+            vtkPoints points = vtkPoints.New();
+            int cnt = 0;
+
+            try
+            {
+                // in case file must be open in another application too use "FileShare.ReadWrite"
+                fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                sr = new StreamReader(fs);
+
+                // Setup the colors array
+                vtkUnsignedCharArray colors = vtkUnsignedCharArray.New();
+                colors.SetNumberOfComponents(3);
+                colors.SetName("Colors");
+                
+                while (!sr.EndOfStream)
+                {
+                    sLineBuffer = sr.ReadLine();
+                    cnt++;
+                    sXYZ = sLineBuffer.Split(chDelimiter, StringSplitOptions.RemoveEmptyEntries);
+                    if (sXYZ == null || sXYZ.Length != 10)
+                    {
+                        MessageBox.Show("data seems to be in wrong format at line " + cnt, "Format Exception", MessageBoxButtons.OK);
+                        return;
+                    }
+                    xyz[0] = double.Parse(sXYZ[0], CultureInfo.InvariantCulture);
+                    xyz[1] = double.Parse(sXYZ[1], CultureInfo.InvariantCulture);
+                    xyz[2] = double.Parse(sXYZ[2], CultureInfo.InvariantCulture);
+                    colors.InsertNextValue(byte.Parse(sXYZ[6], CultureInfo.InvariantCulture));
+                    colors.InsertNextValue(byte.Parse(sXYZ[7], CultureInfo.InvariantCulture));
+                    colors.InsertNextValue(byte.Parse(sXYZ[8], CultureInfo.InvariantCulture));
+                    points.InsertNextPoint(xyz[0], xyz[1], xyz[2]);
+                }
+                
+                vtkPolyData polydata = vtkPolyData.New();
+                polydata.SetPoints(points);
+                polydata.GetPointData().SetScalars(colors);
+                vtkVertexGlyphFilter glyphFilter = vtkVertexGlyphFilter.New();
+                glyphFilter.SetInputConnection(polydata.GetProducerPort());
+
+                // Visualize
+                vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
+                mapper.SetInputConnection(glyphFilter.GetOutputPort());
+                vtkActor actor = vtkActor.New();
+                actor.SetMapper(mapper);
+                actor.GetProperty().SetPointSize(4);
+                // get a reference to the renderwindow of our renderWindowControl1
+                vtkRenderWindow renderWindow = renderWindowControl.RenderWindow;
+                // renderer
+                vtkRenderer renderer = renderWindow.GetRenderers().GetFirstRenderer();
+                // set background color
+                renderer.SetBackground(0.2, 0.3, 0.4);
+                // add our actor to the renderer
+                renderer.AddActor(actor);
+                renderer.ResetCamera();
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "IOException", MessageBoxButtons.OK);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                    sr.Dispose();
+                    sr = null;
+                }
+            }
+        }
+
+        public void SimplePointsReader(RenderWindowControl renderWindowControl)
+        {
+            // Path to vtk data must be set as an environment variable
+            // VTK_DATA_ROOT = "C:\VTK\vtkdata-5.8.0"
+            vtkTesting test = vtkTesting.New();
+            string root = test.GetDataRoot();
+            string filePath = @"C:\Users\Notebook\Desktop\ImageDataset_SceauxCastle-master\pokus.txt";
+
+            vtkSimplePointsReader reader = vtkSimplePointsReader.New();
             reader.SetFileName(filePath);
-            reader.SetDataByteOrderToBigEndian();
             reader.Update();
-            Debug.WriteLine("NumberOfPieces: " + reader.GetOutput().GetNumberOfPieces());
-
             // Visualize
             vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
             mapper.SetInputConnection(reader.GetOutputPort());
-            mapper.SetScalarRange(4, 9);
-            mapper.SetPiece(1);
-
             vtkActor actor = vtkActor.New();
             actor.SetMapper(mapper);
             actor.GetProperty().SetPointSize(4);
-            actor.GetProperty().SetColor(1, 0, 0);
-            // get a reference to the renderwindow of our renderWindowControl1
             vtkRenderWindow renderWindow = renderWindowControl1.RenderWindow;
-            // renderer
             vtkRenderer renderer = renderWindow.GetRenderers().GetFirstRenderer();
-            // set background color
             renderer.SetBackground(0.2, 0.3, 0.4);
-            // add our actor to the renderer
             renderer.AddActor(actor);
+            renderer.ResetCamera();
         }
 
         public void ReadPLY(RenderWindowControl renderWindowControl)
@@ -84,7 +157,7 @@ namespace Bakalárska_práca
             // VTK_DATA_ROOT = "C:\VTK\vtkdata-5.8.0"
             vtkTesting test = vtkTesting.New();
             string root = test.GetDataRoot();
-            string filePath = @"C:\Users\Notebook\Desktop\ImageDataset_SceauxCastle-master\Obj.obj";
+            string filePath = @"C:\Users\Notebook\Desktop\ImageDataset_SceauxCastle-master\pokus.ply";
             vtkOBJReader reader = vtkOBJReader.New();
             reader.SetFileName(filePath);
             reader.Update();
@@ -103,57 +176,11 @@ namespace Bakalárska_práca
             renderer.SetBackground(0.2, 0.3, 0.4);
             // add our actor to the renderer
             renderer.AddActor(actor);
-
-            //vtkPoints points = vtkPoints.New();
-            //    points.InsertNextPoint(0, 1000,20000);
-
-            //    vtkVertex vertex = vtkVertex.New();
-            //    vertex.GetPointIds().SetId(0, 0);
-
-            //    vtkCellArray vertices = vtkCellArray.New();
-            //    vertices.InsertNextCell(vertex);
-
-            //    vtkPolyData polydata = vtkPolyData.New();
-            //    polydata.SetPoints(points);
-            //    polydata.SetVerts(vertices);
-
-            //    // Visualize
-            //    vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
-            //    mapper.SetInputConnection(polydata.GetProducerPort());
-            //    vtkActor actor = vtkActor.New();
-            //    actor.SetMapper(mapper);
-            //    actor.GetProperty().SetPointSize(4);
-            //    vtkRenderWindow renderWindow = renderWindowControl.RenderWindow;
-            //    vtkRenderer renderer = renderWindow.GetRenderers().GetFirstRenderer();
-            //    renderer.SetBackground(0.2, 0.3, 0.4);
-            //    // Add the actor to the scene
-            //    renderer.AddActor(actor);
             renderer.ResetCamera();
         }
+
         public void Point(RenderWindowControl renderWindowControl)
         {
-            // source object
-            vtkSphereSource SphereSource = vtkSphereSource.New();
-            SphereSource.SetRadius(0.5);
-            // mapper
-            vtkPolyDataMapper SphereMapper = vtkPolyDataMapper.New();
-            SphereMapper.SetInputConnection(SphereSource.GetOutputPort());
-            // actor
-            vtkActor SphereActor = vtkActor.New();
-            SphereActor.SetMapper(SphereMapper);
-            // get a reference to the renderwindow of our renderWindowControl1
-            vtkRenderWindow RenderWindow = renderWindowControl.RenderWindow;
-            // get a reference to the renderer
-            vtkRenderer Renderer = RenderWindow.GetRenderers().GetFirstRenderer();
-            // set background color
-            Renderer.SetBackground(0.2, 0.3, 0.4);
-            // add actor to the renderer
-            Renderer.AddActor(SphereActor);
-            // ensure all actors are visible (in this example not necessarely needed,
-            // but in case more than one actor needs to be shown it might be a good idea)
-            Renderer.ResetCamera();
-
-
             // Path to vtk data must be set as an environment variable
             // VTK_DATA_ROOT = "C:\VTK\vtkdata-5.8.0"
             vtkTesting test = vtkTesting.New();
